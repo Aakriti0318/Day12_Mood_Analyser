53f1d601fe08429efd29f878a0dabb21c990d19b
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 18:36:38 +0530

    uc1 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // TC 1.1: Sad Mood
         MoodAnalyser analyser = new MoodAnalyser("I am in a sad mood");
         System.out.println("UC1 - TC1.1 - Mood: " + analyser.analyseMood()); // Output: SAD
 
