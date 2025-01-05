422592d85f16896a5e51643d69babf2c2b672aa9
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 19:49:51 +0530

    uc3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,20 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // TC 3.1: EMPTY Mood
         try {
-            MoodAnalyser analyser = new MoodAnalyser(null);
-            System.out.println("UC2 - TC2.1 - Mood: " + analyser.analyseMood());
+            MoodAnalyser analyser = new MoodAnalyser("");
+            System.out.println("UC3 - TC3.1 - Mood: " + analyser.analyseMood());
         } catch (MoodAnalysisException e) {
-            System.out.println("UC2 - TC2.1 - Exception: " + e.getType() + " - " + e.getMessage());
+            System.out.println("UC3 - TC3.1 - Exception: " + e.getType() + " - " + e.getMessage());
+        }
+
+        // TC 3.2: Valid Mood
+        try {
+            MoodAnalyser analyser = new MoodAnalyser("I am in a happy mood");
+            System.out.println("UC3 - TC3.2 - Mood: " + analyser.analyseMood()); // Output: HAPPY
+        } catch (MoodAnalysisException e) {
+            System.out.println("UC3 - TC3.2 - Exception: " + e.getType() + " - " + e.getMessage());
         }
     }
 }
