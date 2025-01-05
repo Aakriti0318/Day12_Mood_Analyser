df91ac7e0dddc462ffa5216a18d0cdfe2a402fa4
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 19:24:57 +0530

    uc2 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,11 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // TC 1.1: Sad Mood
-        MoodAnalyser analyser = new MoodAnalyser("I am in a sad mood");
-        System.out.println("UC1 - TC1.1 - Mood: " + analyser.analyseMood()); // Output: SAD
-
-        // TC 1.2: Any other Mood
-        analyser = new MoodAnalyser("I am in any mood");
-        System.out.println("UC1 - TC1.2 - Mood: " + analyser.analyseMood()); // Output: HAPPY
-
+        try {
+            MoodAnalyser analyser = new MoodAnalyser(null);
+            System.out.println("UC2 - TC2.1 - Mood: " + analyser.analyseMood());
+        } catch (MoodAnalysisException e) {
+            System.out.println("UC2 - TC2.1 - Exception: " + e.getType() + " - " + e.getMessage());
+        }
     }
 }
